62d04a1932a04d58d6c32fcf8e5f0cd0b9a5d3a7
wendyyuwm <bluehyacinthus@163.com>
2016-08-01 21:03:34 +0800

    Modify error process

--- SocketServerTCP_client.py
+++ SocketServerTCP_client.py
@@ -1,4 +1,6 @@
-from socket import *
+#encoding:utf-8
+# from socket import *
+import socket
 import sys
 import threading
 
@@ -8,21 +10,29 @@ BUF_SIZE = 1600
 def recv(client, recv_sem):
     first_time = True
     while True:
-        recv_data = client.recv(BUF_SIZE)
-        if first_time:
-            recv_sem.release()
-            first_time = False
-        if not recv_data:
-            break
-        print recv_data.strip()
+        try:
+            recv_data = client.recv(BUF_SIZE)
+            if first_time:
+                recv_sem.release()
+                first_time = False
+            if not recv_data:
+                break
+            print recv_data.strip()
+        except socket.error as arg:
+            (errno, err_msg) = arg
+            if errno == 9:
+                break
+            print "Failed: %s, errno = %d" % (err_msg, errno)
+            sys.exit()
+
 
 
 def main():
     host = 'localhost'
     port = 1234
     addr = (host, port)
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     try:
-        client = socket(AF_INET, SOCK_STREAM)
         client.connect(addr)
         recv_semaphore = threading.Semaphore(value=0)
         recv_thread = threading.Thread(target=recv, args=(client, recv_semaphore))
@@ -35,9 +45,11 @@ def main():
             client.send('%s' % send_data)
         client.close()
 
-    except error, arg:
+    except socket.error as arg:
+        print arg
         (errno, err_msg) = arg
         print "Failed: %s, errno = %d" % (err_msg, errno)
+        client.close()
         sys.exit()
 
 
--- SocketServerTCP_server.py
+++ SocketServerTCP_server.py
@@ -1,103 +1,101 @@
+# coding = utf-8
 from socket import *
 import threading
 import sys
 
 BUF_SIZE = 1600
+TEMP_DIR = {}
 
 
-def send(client_name_sock):
+def send(name_sock_map):
     while True:
+        send_data = raw_input("")
+        if send_data.find(":") == -1:
+            print("Format Error!")
+            break
+        name = send_data.split(":")[0]
+        if name not in name_sock_map.keys():
+            print("Client is not OnLine\n")
+            break
+        sock = name_sock_map[name]
         try:
-            send_data = raw_input("")
-            name = send_data.split(":")[0]
-            if name in client_name_sock.keys():
-                sock = client_name_sock[name]
-                sock.send(send_data.split(":")[1])
-
+            sock.send(send_data.split(":")[1])
         except error as arg:
             (errno, err_msg) = arg
             print "Error: %s, errno = %d" % (err_msg, errno)
-            # sys.exit()   modify error type
-
-        finally:
-            sock.close()
+            if errno == 10054:
+                sock.close()
+            break
 
 
 def recv(client_sock):
     while True:
         try:
-            recv_data = client_sock.recv(BUF_SIZE).strip()
+            recv_data = client_sock.recv(BUF_SIZE)
             if not recv_data:
                 break
-            print recv_data
-
+            print recv_data.strip()
         except error as arg:
             (errno, err_msg) = arg
             print "Error: %s, errno = %d" % (err_msg, errno)
-            # sys.exit() modify error type
+            if errno == 10054:
+                client_sock.close()
+                break
 
-        finally:
+
+def user_password(client_sock):
+    try:
+        client_dir = {}
+        client_sock.send("New Client? (Y)es or (N)o: ")
+        new_user = client_sock.recv(1024).strip()
+        client_sock.send("Enter the client name: ")
+        name = client_sock.recv(1024).strip()
+        client_sock.send("Please enter the password: ")
+        password = client_sock.recv(1024).strip()
+        TEMP_DIR[name] = client_sock
+        if new_user.upper() == "Y":
+            client_dir[name] = password
+            return True
+        elif new_user.upper() == "N":
+            if client_dir[name] == password:
+                return True
+            else:
+                print "Wrong PassWord!!\t\t...Quit...\n"
+                return False
+        else:
+            print "Enter the wrong command\t\t...Quit...\n"
+            return False
+
+    except error as arg:
+        (errno, err_msg) = arg
+        print "Error: %s, errno = %d" % (err_msg, errno)
+        if errno == 10054:
             client_sock.close()
-
-def user_password(client_sock, ):
-    temp_dir= {}
-    client_sock.send("New Client? (Y)es or (N)o: ")
-    new_user = client_sock.recv(1024).strip()
-    client_conn.send("Enter the client name: ")
-    name = client_sock.recv(1024).strip()
-    client_conn.send("Please enter the password: ")
-    password = client_sock.recv(1024).strip()
-    temp_dir[name] = client_sock
-    if new_user.upper() == "Y":
-        client_dir[name] = password
+        else:
+            return False
 
 
 def main():
     host = "localhost"
     port = 1234
     server_addr = (host, port)
+    s = socket(AF_INET, SOCK_STREAM)
     try:
-        client_dir = {}
-        temp_dir = {}
-        s = socket(AF_INET, SOCK_STREAM)
         s.bind(server_addr)
-        s.listen(50)
-        while True:
-            (client_conn, client_addr) = s.accept()
-            client_conn.send("New Client? (Y)es or (N)o: ")
-            new_user = client_conn.recv(1024).strip()
-            client_conn.send("Enter the client name: ")
-            name = client_conn.recv(1024).strip()
-            client_conn.send("Please enter the password: ")
-            password = client_conn.recv(1024).strip()
-            temp_dir[name] = client_conn
-            if new_user.upper() == "Y":
-                client_dir[name] = password
-                send_thread = threading.Thread(target=send, args=(temp_dir, ))
-                send_thread.start()
-                recv_thread = threading.Thread(target=recv, args=(client_conn,))
-                recv_thread.start()
-            elif new_user.upper() == "N":
-                # print client_dir
-                if client_dir[name] == password:
-                    send_thread = threading.Thread(target=send, args=(temp_dir, ))
-                    send_thread.start()
-                    recv_thread = threading.Thread(target=recv, args=(client_conn,))
-                    recv_thread.start()
-                else:
-                    print "Wrong PassWord!!\t\t...Quit...\n"
-                    sys.exit()
-            else:
-                print "Enter the wrong command\t\t...Quit...\n"
-                sys.exit()
-            print client_dir
-            del temp_dir
-
-    except error, arg:
+    except error as arg:
         (errno, err_msg) = arg
-        print "Error: %s, errno = %d" % (err_msg, errno)
+        print "Bind Failed: %s, errno = %d" % (err_msg, errno)
         sys.exit()
-
+    s.listen(5)
+    while True:
+        (client_conn, client_addr) = s.accept()
+        print client_conn
+        if user_password(client_conn) is True:
+            print TEMP_DIR
+            send_thread = threading.Thread(target=send, args=(TEMP_DIR, ))
+            send_thread.start()
+            recv_thread = threading.Thread(target=recv, args=(client_conn,))
+            recv_thread.start()
     s.close()
 
 if __name__ == "__main__":
